a218518a8919b97207ccb14dd76c53423bb86d44
JD Pierce <brkfstmnchr@gmail.com>
2016-08-08 19:07:53 -0700

    added SerialDeviceFactoryTests unit test class

---
+++ KA3005P.Test/SerialDeviceTests/SerialDeviceFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using KA3005P.Proxy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KA3005P.Test
+{
+    [TestClass]
+    public class SerialDeviceFactoryTests
+    {
+        [TestMethod]
+        public void TestSerialDeviceEnumeration()
+        {
+            SerialDeviceFactory factory = new SerialDeviceFactory();
+            List<string> devices = factory.Enumerate();
+            foreach(string device in devices)
+                Console.WriteLine(device);
+            return;
+        }
+    }
+}
